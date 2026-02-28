f710028fe8604262fb8e51cc29cd9828c2b1a173
Simone Avagliano <avaglianosimonegmail.com>
2026-03-01 00:44:32 +0100

    Added inventory tests

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,20 +1,24 @@
+from enum import Enum
 
 from pages.base_page import BasePage
 
 # TODO 1: Define which tests could be performed on the main page
 # TODO 2: Define what helper methods could be needed in the main page for the tests
 
+class HamburgerOptions(Enum):
+    ALL_ITEMS = ("id","inventory_sidebar_link")
+    ABOUT = ("id","about_sidebar_link")
+    LOGOUT = ("id","logout_sidebar_link")
+    RESET_APP_STATE = ("id","reset_sidebar_link")
+
 class InventoryPage(BasePage):
     TITLE = ("class_name","app_logo")
 
     HAMBURGER = ("id","react-burger-menu-btn")
 
-    ALL_ITEMS_LINK = ("id","inventory_sidebar_link")
-    ABOUT_LINK = ("id","about_sidebar_link")
-    LOGOUT_LINK = ("id","logout_sidebar_link")
-    RESET_APP_STATE_LINK = ("id","reset_sidebar_link")
-
     CART = ("id","shopping_cart_container")
+    CART_BADGE = ("class_name","shopping_cart_badge")
+
     FILTER = ("class_name", "select_container")
     INVENTORY = ("xpath","//*[@id='inventory_container']")
     
@@ -25,10 +29,25 @@ class InventoryPage(BasePage):
         return self.find(*self.HAMBURGER)
     
     def open_hamburger(self):
+        self.wait_for_visible(*self.HAMBURGER)
         self.click(*self.HAMBURGER)
-    
+
+    def click_hamburger_option(self, option: HamburgerOptions):
+        self.open_hamburger()
+        self.click(*option.value)
+        
+    def get_hamburger_option(self, option: HamburgerOptions):
+        self.open_hamburger()
+        return self.find(*option.value)
+            
     def go_to_cart(self):
         self.click(*self.CART)
 
+    def get_cart_badge_count(self):
+        try:
+          return int(self.find(*self.CART_BADGE).text)
+        except:
+            return 0
     
-    
+    def add_backpack_to_cart(self):
+        self.click("id", "add-to-cart-sauce-labs-backpack")
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,7 +1,7 @@
 import pytest
 
 from tests.base_test import BaseTest
-from pages.inventory_page import InventoryPage
+from pages.inventory_page import InventoryPage, HamburgerOptions
 from config.config import Config
 
 class TestInventory(BaseTest):
@@ -25,4 +25,34 @@ class TestInventory(BaseTest):
 
         assert "cart.html" in self.inventory_page.url
 
+    def test_open_hamburger(self):
+        self.inventory_page.open_hamburger()
+
+        link = self.inventory_page.get_hamburger_option(HamburgerOptions.ALL_ITEMS)
+        assert link
+
+    def test_hamburger_all_items(self):
+        self.inventory_page.go_to_cart() # to make sure we are not on the inventory page already
+
+        self.inventory_page.click_hamburger_option(HamburgerOptions.ALL_ITEMS)
+
+        assert "inventory.html" in self.inventory_page.url
+
+    def test_hamburger_about(self):
+        self.inventory_page.click_hamburger_option(HamburgerOptions.ABOUT)
+
+        assert "saucelabs.com" in self.inventory_page.url
+
+    def test_hamburger_logout(self):
+        self.inventory_page.click_hamburger_option(HamburgerOptions.LOGOUT)
+
+        assert self.inventory_page.url == self.config.BASE_URL
+
+    def test_hamburger_reset_app_state(self):
+        self.inventory_page.add_backpack_to_cart()
+
+        assert self.inventory_page.get_cart_badge_count() > 0 # to make sure that the badge is visible
+
+        self.inventory_page.click_hamburger_option(HamburgerOptions.RESET_APP_STATE)
         
+        assert self.inventory_page.get_cart_badge_count() == 0
